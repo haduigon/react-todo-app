e18c6c444320cc9c7aeb606dabe7613efed91f46
haduigon <haduigon@gmail.com>
2024-06-06 16:04:12 +0300

    main commit

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,14 @@ import { createRoot } from 'react-dom/client';
 
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
-import './index.scss';
+import './styles/index.scss';
+import { TodoProvider } from './components/TodoContext';
 
 import { App } from './App';
 
-createRoot(document.getElementById('root') as HTMLDivElement).render(<App />);
+createRoot(document.getElementById('root') as HTMLDivElement)
+  .render(
+    <TodoProvider>
+      <App />
+    </TodoProvider>,
+  );
